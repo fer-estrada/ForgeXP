d7c83a6119036700d6ff572381428e9142d8cf2c
Nathaniel Downey <downeyn21@gmail.com>
2025-04-18 12:43:16 -0400

    file submit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,14 +6,15 @@ import AllGames from "./components/Games/AllGames";
 import SingleGame from "./components/Games/SingleGame";
 import Login from "./components/Login";
 import Register from "./components/Register";
-
+import { Upload } from "lucide-react";
+import Uploads from "./components/Account/Uploads";
 function App() {
   const [token, setToken] = useState(null);
 
   return (
     <div className="min-h-screen bg-gray-950 text-white">
       {/* <div className="max-w-[2900px] mx-auto px-4"> */}
-      <NavBar />
+      {/* <NavBar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login setToken={setToken} />} />
@@ -23,8 +24,9 @@ function App() {
         <Route path="/games/:id" element={<SingleGame />} />
 
         {/* Add other routes later, set "/" to Homepage so that when user goes to forgexp.com it routes to HomePage */}
-      </Routes>
+      { /*</Routes> */}
       {/* </div> */}
+      <Uploads/>
     </div>
   );
 }
--- front-end/src/components/Account/Uploads.jsx
+++ front-end/src/components/Account/Uploads.jsx
@@ -4,7 +4,10 @@
 const Uploads = () => {
     return ( 
     <div>
-        
+        <form method="POST" action="/upload" encType="multipart/from-data">
+            <input type="file" name="image" />
+            <input type="submit" />
+        </form>
     </div> 
     );
 }
